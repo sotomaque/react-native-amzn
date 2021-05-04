9296b67718ec25e910a01daa536bb9f54663d58e
Enrique <sotomaque@gmail.com>
2021-05-04 15:27:06 -0700

    feat: bottom sheet WIP

--- src/screens/AddressFormScreen/AddressFormScreen.tsx
+++ src/screens/AddressFormScreen/AddressFormScreen.tsx
@@ -1,15 +1,29 @@
 import { Picker } from '@react-native-picker/picker';
-import React, { useState } from 'react';
-import { View, Text, Dimensions, Pressable } from 'react-native';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { View, Text, Dimensions, Pressable, TextInput } from 'react-native';
 import styles from './styles';
 import countryList from 'country-list';
 import Icon from 'react-native-vector-icons/dist/AntDesign';
+import BottomSheet from '@gorhom/bottom-sheet';
 
 const AddressFormScreen = () => {
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
   const countries = countryList.getData();
   const [country, setCountry] = useState(countries[0].name);
-  const [showPicker, setShowPicker] = useState(true);
-  console.log(country);
+  const [showPicker, setShowPicker] = useState(false);
+  const [fullName, setFullName] = useState('');
+  const [streetAddress, setStreetAddress] = useState('');
+  const [secondaryAddress, setSecondaryAddress] = useState('');
   return (
     <View
       style={{
@@ -53,7 +67,19 @@ const AddressFormScreen = () => {
           </Picker>
         </View>
       )}
-      <View>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheet>
+
+      {/* Country Input / Label */}
+      <View style={{ marginBottom: 10 }}>
         <Text style={{ fontSize: 16, marginBottom: 5, fontWeight: '600' }}>
           Country
         </Text>
@@ -82,13 +108,70 @@ const AddressFormScreen = () => {
       </View>
 
       {/* Full Name */}
-      <View style={styles.row}>
-        {/* Label */}
-        <Text>Full Name (first and last)</Text>
-        {/* Input */}
+      <View style={{ marginVertical: 10 }}>
+        <Text style={{ fontSize: 16, marginBottom: 5, fontWeight: '600' }}>
+          Full Name (First and Last Name)
+        </Text>
+
+        <TextInput
+          style={{
+            fontSize: 18,
+            paddingLeft: 10,
+            borderWidth: 1,
+            borderColor: 'gray',
+            height: 45,
+            backgroundColor: 'white',
+            borderRadius: 5,
+          }}
+          value={fullName}
+          onChangeText={text => setFullName(text)}
+          clearButtonMode="always"
+          autoCorrect={false}
+        />
       </View>
-      <View style={styles.row}>
-        <Text>dummy text</Text>
+
+      {/* Address */}
+      <View style={{ marginVertical: 10 }}>
+        <Text style={{ fontSize: 16, marginBottom: 5, fontWeight: '600' }}>
+          Address
+        </Text>
+
+        <TextInput
+          style={{
+            fontSize: 18,
+            paddingLeft: 10,
+            borderWidth: 1,
+            borderTopLeftRadius: 5,
+            borderTopRightRadius: 5,
+            borderColor: 'gray',
+            height: 45,
+            backgroundColor: 'white',
+          }}
+          placeholder="Street address or P.O. Box"
+          value={streetAddress}
+          onChangeText={text => setStreetAddress(text)}
+          clearButtonMode="always"
+          autoCorrect={false}
+        />
+        <TextInput
+          style={{
+            fontSize: 18,
+            paddingLeft: 10,
+            borderBottomWidth: 1,
+            borderLeftWidth: 1,
+            borderRightWidth: 1,
+            borderBottomLeftRadius: 5,
+            borderBottomRightRadius: 5,
+            borderColor: 'gray',
+            height: 45,
+            backgroundColor: 'white',
+          }}
+          placeholder="Apt, Suite, Unit, Building (optional)"
+          value={secondaryAddress}
+          onChangeText={text => setSecondaryAddress(text)}
+          clearButtonMode="always"
+          autoCorrect={false}
+        />
       </View>
     </View>
   );
